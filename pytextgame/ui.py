5e1b37b18b5a51fcde602071555a64f0e72c595c
John Stilley <thejollysin@gmail.com>
2015-09-09 18:39:53 -0700

    down the rabbit hole: a new project

--- pytextgame/ui.py
+++ pytextgame/ui.py
@@ -1,3 +1,8 @@
+'''A simple UI for Text Games in Python,
+built on a PyGame-based curses emulator
+'''
+
+from pytextgame import cursesemu
 
 
 class TextUI:
@@ -5,3 +10,146 @@ class TextUI:
     def __init__(self, num_rows, num_cols):
         self._num_rows = num_rows
         self._num_cols = num_cols
+
+    def main(self):
+        '''Use cursesemu to start the PyGame display
+        '''
+        cursesemu.wrapper(lambda stdscr: self.set_up_and_run(stdscr),
+                          self._num_rows, self._num_cols)
+
+    def set_up_and_run(self, stdscr):
+        '''Set up and run the UI, though this class does not implement
+        the details necessary to do so.
+        '''
+        self.stdscr = stdscr
+        self.init()
+        self.run()
+
+    def get_key(self):
+        '''Get a key that has been pressed using the
+        pygame interface.
+        '''
+        return self.stdscr.getch()
+
+    def init(self):
+        raise 'Not implemented'
+
+    def run(self):
+        raise 'Not implemented'
+
+
+class TextGameUI(TextUI):
+
+    def __init__(self, model):
+        TextUI.__init__(self, model.num_rows, model.num_cols)
+        self.model = model
+        self._null_key = False
+
+    def init(self):
+        pass
+
+    def run(self):
+        '''just filling the superclasses method with game logic'''
+        self.do_game()
+
+    def do_game(self):
+        '''master method to handle rendering displaying,
+        modifying game state, and handling user input
+        '''
+        self.model.start()
+
+        while self.model.is_running():
+            self.model.do_turn()
+            self.do_turn()
+
+    def prepare_turn(self):
+        pass
+
+    def do_turn(self):
+        '''Do a single turn of render-input
+        Allow to render using memoized data from last pull,
+        if no user-input is given.
+        '''
+        # pull information necessary to render
+        self.prepare_turn()
+
+        # render screen
+        if self._null_key:
+            # if only null user input, render using memoized screen
+            self.display_last()
+            self._null_key = False
+        else:
+            # render fresh screen
+            self.display()
+
+        # look for user input and perform necessary actions
+        self.act()
+
+    def do_scores(self):
+        # TODO: Just scores? We need better support for multiple menus.
+        pass
+
+    def display(self):
+        '''display every window in this UI'''
+        # re-draw every sub-window
+        for window in self.displayed_windows():
+            window.display()
+
+        self.stdscr.refresh()
+
+    def display_last(self):
+        '''In the situation where a null input was recieved from user
+        (window resize, etc), simply re-draw the last screen
+        using the memoized data
+        '''
+        for window in self.displayed_windows():
+            window.stdscr.refresh()
+
+    def create_window(self, kind, rect, border=True):
+        '''Helper method to add a subwindow to the screen'''
+        return kind(self, self.stdscr, self.model, rect, border)
+
+    def act(self):
+        '''wait for user input and perform any actions necessary
+        if a null key is returned, no actions are necessary
+        '''
+        acted = False
+
+        # TODO: This and the 'cursesemu.getch' need to be combined.
+        #       We want to allow for the GUI to change even when the user doesn't press a key.
+        while not acted:
+            key = self.get_key()
+
+            # null key is for screen re-sizing, quiting the game, etc
+            if key == cursesemu.NULL_KEY:
+                self._null_key = True
+                return
+
+            self.display()
+
+            actions = self.model.available_actions()
+            for action in actions:
+                if self.key_for(action) == key:
+                    acted = action.execute()
+
+    # TODO: Make sure all of these are used!
+    def char_for(self, action):
+        # TODO: Used in display, but why?
+        raise 'Not implemented'
+
+    # TODO: this is too entagled with Phoenix?
+    def key_for(self, action):
+        raise 'Not implemented'
+
+    # TODO: this is too entagled with Phoenix
+    def group_for(self, action):
+        '''Get an action name from the GROUP dict'''
+        raise 'Not implemented'
+
+    def displayed_windows(self):
+        '''Return a list of subwindows'''
+        raise 'Not implemented'
+
+    def window(self, name):
+        '''Get a window based on it's name key'''
+        raise 'Not implemented'
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='pytextgame',
-    version='0.0.1',
+    version='0.0.2',
     description="Cross-Platform Python Text Game Library",
     long_description=readme + '\n\n' + history,
     author="John Stilley",
